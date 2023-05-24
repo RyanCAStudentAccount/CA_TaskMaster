e5c1d3f66cd3e16ee7aa4c5a96b622afabcf3ed8
ryanStudentAccount <121059879+ryanStudentAccount@users.noreply.github.com>
2023-05-24 14:00:50 +0100

    Create buildscript.sh

---
+++ buildscript.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Set up environment variables
+
+# Install MAUI workloads
+dotnet workload install maui
+dotnet workload install maui-android
+dotnet workload install maui-ios
+dotnet workload install maccatalyst
+
+# Restore project dependencies
+dotnet restore
+
+# Build the project
+dotnet build --no-restore
+
+# Run tests
+dotnet test --no-build --verbosity normal --framework net6.0-windows
+
+# Publish
+dotnet publish --no-build --configuration Release --output ./publish
+
